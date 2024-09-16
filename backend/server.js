a790161ed24e28f0bad61258aba8311343920bc3
MianajiAli <87234097+MianajiAli@users.noreply.github.com>
2024-09-16 16:40:26 +0330

    Fix directory

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const dotEnv = require('dotenv');
 dotEnv.config({ path: './config.env' });
 
 // Use environment variables for the port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Start the server
 app.listen(PORT, () => {
