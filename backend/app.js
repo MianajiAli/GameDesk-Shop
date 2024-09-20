896802a5a5467d3f1cfd0e07b5b5e128f6141917
MianajiAli <87234097+MianajiAli@users.noreply.github.com>
2024-09-21 03:11:47 +0330

    add two middlewere

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,10 @@ const productRoutes = require('./routes/productRoutes');
 // const paymentRoutes = require('./routes/paymentRoutes');
 const cartRoutes = require('./routes/cartRoutes'); // Import cart routes
 const dotEnv = require('dotenv');
+const cors = require('cors');
+
+const setHeaders = require("./middlewares/headers.middlewares.js");
+const errorHandler = require("./middlewares/errors.middlewares.js");
 
 // Initialize environment variables
 dotEnv.config();
@@ -21,12 +25,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // This is needed to parse URL-encoded payloads
 app.use(express.static('./storage'));
 
+app.use(setHeaders);
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 // app.use('/api/orders', orderRoutes);
 // app.use('/api/payments', paymentRoutes);
-
+// Error Handler
+app.use(errorHandler)
 // Export the app
 module.exports = app;
--- backend/middlewares/errors.middlewares.js
+++ backend/middlewares/errors.middlewares.js
@@ -1,4 +1,4 @@
-export const errorHandler = (err, req, res, next) => {
+module.exports = errorHandler = (err, req, res, next) => {
   const status = err.statusCode || 500;
   const message = err.message || "خطایی از سمت سرور رخ داده است";
   const data = err.data;
